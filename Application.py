b6fa113859630617e15542dae49e63fa88e533cb
johncarlodeveloepr <2023100305@bulsu.edu.ph>
2025-09-15 11:19:14 +0800

    allow tcp

--- Application.py
+++ Application.py
@@ -172,5 +172,6 @@ if __name__ == "__main__":
 
         
 
-    socketio.run(app, debug=True, host="0.0.0.0", port=5000, use_reloader=False)
+    socketio.run(app, host="0.0.0.0", port=5000, debug=False, use_reloader=False)
+
 
