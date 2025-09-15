382a4abe92f5aeec5cf1bec934e0e5826a0e1b4a
johncarlodeveloepr <2023100305@bulsu.edu.ph>
2025-09-15 10:45:24 +0800

    remove poppler path and fix syntax error

--- Application.py
+++ Application.py
@@ -172,4 +172,5 @@ if __name__ == "__main__":
 
         
 
-    socketio.run(app, debug=True, port=5000, use_reloader=False)
+    socketio.run(app, debug=True, host="0.0.0.0", port=5000, use_reloader=False)
+
