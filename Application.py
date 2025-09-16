ffc81071b87239ee7fb9fcac8cd6ce2292b48621
johncarlodeveloepr <2023100305@bulsu.edu.ph>
2025-09-16 14:10:43 +0800

    set dynamic port

--- Application.py
+++ Application.py
@@ -174,8 +174,9 @@ def serve(path):
 
 
 if __name__ == "__main__":
-    import logging
+    import logging, os
     logging.basicConfig(level=logging.INFO)  
+    port = int(os.environ.get("PORT", 5000))  # fallback to 5000 locally
         
     socketio.run(app, host="0.0.0.0", port=5000, debug=False, use_reloader=False)
 
